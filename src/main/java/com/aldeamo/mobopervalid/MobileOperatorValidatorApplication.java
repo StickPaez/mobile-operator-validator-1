374aa3196996f045060dd95641e07a018671d7ba
Laura López <laura.lopez@aldeamo.com>
2021-06-09 10:06:53 -0500

    Initial Commit

---
+++ src/main/java/com/aldeamo/mobopervalid/MobileOperatorValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.aldeamo.mobopervalid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileOperatorValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileOperatorValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aldeamo/mobopervalid/MobileOperatorValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aldeamo.mobopervalid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileOperatorValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
