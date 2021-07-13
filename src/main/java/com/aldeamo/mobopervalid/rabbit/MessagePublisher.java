93816221cb3f30e9b007cf7da6da2784acb455c5
Carlos Parra <carlos.parra@aldeamo.com>
2021-07-13 08:59:41 -0500

    Fix use of MessagePublisherConfiguration

--- src/main/java/com/aldeamo/mobopervalid/rabbit/MessagePublisher.java
+++ src/main/java/com/aldeamo/mobopervalid/rabbit/MessagePublisher.java
@@ -4,16 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.aldeamo.mobopervalid.model.ValidationResult;
-import com.aldeamo.mobopervalid.rabbit.config.RabbitMqConfiguration;
+import com.aldeamo.mobopervalid.rabbit.config.MessagePublisherConfiguration;
 
 @Component
 public class MessagePublisher
 {
     @Autowired
-    private RabbitMqConfiguration rabbitMqConfiguration;
+    private MessagePublisherConfiguration messagePublisherConfiguration;
 
     public void sendResult(ValidationResult result, String queueName)
     {    	
-    	rabbitMqConfiguration.rabbitTemplate().convertAndSend(queueName, result);
+    	messagePublisherConfiguration.rabbitTemplate().convertAndSend(queueName, result);
     } 
 }
